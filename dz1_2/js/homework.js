c6e702a194a26c57ffd6f3e874c96c2a7e289540
Maxstar2103 <107531209+Maxstar2103@users.noreply.github.com>
2022-06-24 15:13:59 +0300

    Update homework.js

--- dz1_2/js/homework.js
+++ dz1_2/js/homework.js
@@ -1,37 +1,73 @@
+/*
 let arr = []
 
 function getNumToArr(numSatart, numFinish, arrow) {
 
 	for (let i = numSatart; i <= numFinish; i++) {
 		arrow.push(i)
-	}
-
-}
-
-function removeNegativeNum(arrow) {
-	for (let i = 0; i <= arrow.length; i++) {
-		arrow.shift(-i)
-	}
-}
-
-function removeDivisibleNum(arrow, num) {
-	for (let i = 0; i <= arrow.length; i++) {
-		if (arrow[i] % num === 0) {
-			arrow.splice(i, 1)
+		if (arrow(-i)) {
+			arrow.shift(-i)
 		}
 	}
 }
+getNumToArr(-50, 50, arr)
+console.log(arr)
+*/
+/*
+ function removeNegativeNum(arrow) {
+	  for (let i = 0; i <= arrow.length; i++) {
+			 arrow.shift(-i)
+	  }
+ }
+ */
 
-function getContent(arrow) {
-	for (let i = 0; i <= arrow.length - 1; i++) {
+/*
+ function removeDivisibleNum(arrow, num) {
+	  for (let i = 0; i <= arrow.length; i++) {
+			 if (arrow[i] % num === 0) {
+				  arrow.splice(i, 1)
+			 }
+	  }
+ }
+*/
+/*
+ function getContent(arrow) {
+	  for (let i = 0; i <= arrow.length - 1; i++) {
 
-		document.getElementById('app').textContent += arrow[i]
+			 document.getElementById('app').textContent += arrow[i]
 
 	}
+ }
+*/
+
+// getNumToArr(-50, 50, arr)
+// removeNegativeNum(arr)
+// removeDivisibleNum(arr, 11)
+// getContent(arr)
+
+
+
+
+/*
+Чому функція працює тільки для -і в масиві???
+
+let arr = []
+let num = 11
+
+function getNumToArr(numSatart, numFinish, arrow) {
+
+	for (let i = numSatart - 1; i <= numFinish; i++) {
+
+		if (arrow[i] % num === 0) {
+			arrow.splice(i, 1)
+		}
+		else {
+			arrow.push(i)
+		}
+	}
+
 }
-
-
 getNumToArr(-50, 50, arr)
-removeNegativeNum(arr)
-removeDivisibleNum(arr, 11)
-getContent(arr)
+console.log(arr)
+*/
+
