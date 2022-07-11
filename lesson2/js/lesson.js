0f9cd2103c73dbfeef65364f1d7a1f982159eb2f
Maxstar2103 <107531209+Maxstar2103@users.noreply.github.com>
2022-07-11 18:24:26 +0300

    lesson2

--- lesson2/js/lesson.js
+++ lesson2/js/lesson.js
@@ -24,8 +24,8 @@ function readJSON(file) {
 };
 const info = JSON.parse(readJSON('items.json'))
 
-console.log(info)
 
+let infoImageContainer
 const renderList = () => {
 
 
@@ -40,35 +40,72 @@ const renderList = () => {
 		divBlock.appendChild(div)
 		app.appendChild(divBlock)
 
-
-
-
-
-
-
+		divBlock.addEventListener('click', () => renderInfoImage(item))
 
 	}
+	removeBlock()
+	addLike()
 
+}
+const addLike = () => {
+	const spanLike = document.getElementById('span-like')
+	const spanDiss = document.getElementById('span-diss')
+	const likes = document.querySelectorAll('.left-block__like')
+
+	let numLike = 0
+	let numDissLike = 0
+	likes.forEach((like) => {
+		like.addEventListener('click', (e) => {
+			const target = e.target
+			console.log(target)
+			if (target.closest('.icon-like')) {
+				numLike++
+				spanLike.textContent = numLike
+			} else {
+				numDissLike++
+				spanDiss.textContent = numDissLike
+			}
+
+		})
+	})
+}
+const renderInfoImage = (item) => {
+	infoImageContainer = document.querySelector('.info-image__container')
+	const infoImage = document.getElementById('left-block__picture')
+	infoImageContainer.classList.add('active')
+	// setTimeout(() => infoImageContainer.style.display = 'flex', 300)
+
+	infoImage.src = item.url
+
+	renderComments(item.comments)
+
+
+
+}
+const removeBlock = () => {
+	const rightClose = document.querySelector('.right-block__close')
+	rightClose.addEventListener('click', (e) => {
+		const event = e.target
+		if (!event === rightClose) {
+			return
+		} else {
+			infoImageContainer.classList.remove('active')
+		}
+	})
 }
 renderList()
 
+const renderComments = (list) => {
+	const comments = document.getElementById('right-block__comments')
 
-
-// storageBtn.addEventListener('click', () => {
-// 	storagePopap.classList.toggle('hiden')
-// })
-
-// let count = 0
-// storageLikeBtn.addEventListener('click', () => {
-// 	if (storageLikeBtn) {
-// 		count++
-// 	}
-// 	valueBtn.textContent = count;
-// })
-
-
-
-
+	for (const item of list) {
+		console.log(item)
+		const div = document.createElement('div')
+		div.textContent = item
+		div.className = 'right-block__comment'
+		comments.appendChild(div)
+	}
+}
 
 
 
