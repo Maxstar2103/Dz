96bc092c8e39e9c0d98b62482ead68a21ff44b66
Maxstar2103 <107531209+Maxstar2103@users.noreply.github.com>
2022-07-13 10:14:41 +0300

    app

---
+++ app/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () =>{
+	return(
+		<header className="header">
+		<img src='https://www.vectorkhazana.com/assets/images/products/playboylog.jpg' />
+	</header>
+	)
+}
+
+export default Header
---
+++ app/components/Profile.jsx
@@ -0,0 +1,30 @@
+
+
+const Profile = () =>{
+	return(
+		<div className="content">
+		<div><img src='http://www.teleenergia.ch/manutank/assets/img/demo/7.jpg' /></div>
+		<div> ava + desc</div>
+		<div>
+			My photos
+			<div>
+				New post
+			</div>
+			<div>
+				post 1
+			</div>
+			<div>
+				post 2
+			</div>
+			<div>
+				post 3
+			</div>
+		</div>
+
+
+	</div>
+	)
+}
+
+
+export default Profile
